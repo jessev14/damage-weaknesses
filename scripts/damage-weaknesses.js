b9393b9375ddcaf14267e7576c4e2b44fd96d300
jessev14 <jessevo321@gmail.com>
2024-05-14 15:36:15 -0400

    dnd5e v3 update

--- scripts/damage-weaknesses.js
+++ scripts/damage-weaknesses.js
@@ -20,30 +20,58 @@ Hooks.on('renderActorSheet5e', async (app, [html], appData) => {
             custom: ""
         });
     }
-
     const dw = actor.getFlag(moduleID, 'dw');
-    const dwSet = {
-        bypasses: new Set(dw.bypasses),
-        value: new Set(dw.value),
-        custom: dw.custom
+    const empty = dw.value.length ? false : true;
+
+    let pills = ``;
+    for (const weakness of dw.value) {
+        pills += `
+            <li class="pill maroon ">
+                <span class="label">${CONFIG.DND5E.damageTypes[weakness].label}</span>
+            </li>
+        `;
     }
-    const traits = app._prepareTraits({
-        traits: {
-            dw: dwSet
+    const sheetMode = app._mode === 1 ? 'play': 'edit';
+    if (sheetMode === 'edit') {
+        const dvDiv = html.querySelector('div.pills-group h3.icon a[data-trait="dv"]').closest('div');
+        const dwDiv = document.createElement('div');
+        dwDiv.classList.add('pills-group');
+        if (empty) dwDiv.classList.add('empty');
+        dwDiv.innerHTML = `
+            <h3 class="icon">
+                <i class="fa-solid fa-heart-half-stroke"></i>
+                <span class="roboto-upper">Damage Weaknesses</span>
+                <a class="trait-selector" data-trait="dw" data-tooltip="Configure Damage Weaknesses" aria-label="Configure Damage Weaknesses">
+                    <i class="fas fa-cog"></i>
+                </a>
+            </h3>
+            <ul class="pills">
+                ${pills}
+            </ul>
+        `;
+        const dwSet = {
+            bypasses: new Set(dw.bypasses),
+            value: new Set(dw.value),
+            custom: dw.custom
         }
-    });
+        actor._source.system.traits.dw = dwSet;
+        dwDiv.querySelector('a.trait-selector').addEventListener('click', () => new dwTraitSelector(actor, dwSet).render(true)); 
+        dvDiv.after(dwDiv);
+    } else {
+        const dwDiv = document.createElement('div');
+        dwDiv.classList.add('pills-group');
+        dwDiv.innerHTML = `
+            <h3 class="icon">
+            <i class="fa-solid fa-heart-half-stroke"></i>
+            <span class="roboto-upper">Weaknesses</span>
+            </h3>
+            <ul class="pills">
+                ${pills}
+            </ul>
+        `;
 
-    const dwDiv = document.createElement('div');
-    dwDiv.classList.add('form-group');
-    if (traits.traits.dw.cssClass) dwDiv.classList.add(traits.traits.dw.cssClass);
-    const content = await renderTemplate(`modules/${moduleID}/templates/dw-template.hbs`, traits);
-    dwDiv.innerHTML = content;
-    dwDiv.querySelector('a.trait-selector').addEventListener('click', () => {
-        new dwTraitSelector(actor, dwSet).render(true);
-    });
-
-    const dvDiv = html.querySelector('a.trait-selector[data-trait="dv"]').parentElement;
-    dvDiv.before(dwDiv);
+        html.querySelector('div.right div.top.flexrow').after(dwDiv);
+    }
 });
 
 
@@ -52,30 +80,24 @@ class dwTraitSelector extends dnd5e.applications.actor.TraitSelector {
         actor.system.traits.dw = dwSet;
 
         super(actor, 'dw');
+
+        this.dwSet = dwSet;
     }
 
     get title() {
         return `${this.document.name}: Damage Weaknesses`;
     }
 
-    _getActorOverrides() {
-        this.document._source.system.traits.dw = {
-            value: new Set(this.document.getFlag(moduleID, 'dw').value)
-        };
-
-        return super._getActorOverrides();
-    }
-
     async _updateObject(event, formData) {
         const path = 'system.traits.dw';
         const data = foundry.utils.getProperty(this.document, path);
     
-        this._prepareChoices('choices', 'value', formData);
-        if ( 'bypasses' in data ) this._prepareChoices('bypasses', 'bypasses', formData);
-
+        this._prepareChoices('choices', `${path}.value`, formData);
+        if ( 'bypasses' in data ) this._prepareChoices('bypasses', `${path}.bypasses`, formData);
+       
         const updateData = {
-            bypasses: formData.bypasses,
-            value: formData.value,
+            bypasses: formData['system.traits.dw.bypasses'],
+            value: formData['system.traits.dw.value'],
             custom: formData['system.traits.dw.custom']
         };
     
